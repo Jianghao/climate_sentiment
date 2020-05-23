07b13889f23bb20f6048da23c8bdef443a4b0ed4
Jianghao <wangjh@lreis.ac.cn>
2020-05-23 03:08:06 -0400

    updates

--- script/figure1A/weibo_visual.R
+++ script/figure1A/weibo_visual.R
@@ -7,13 +7,14 @@ setwd("~/Documents/GitHub/climate_sentiment")
 
 library(raster)
 library(rasterVis)
-library(openair)
 library(rgdal)
+library(RColorBrewer)
 
-mapTheme <- rasterTheme(region = openair::openColours(scheme = "jet", n = 100))
+mapTheme <- rasterTheme(region = rev(brewer.pal(11, "RdYlBu")))
 bound <- readOGR("data/shp/china-with-illustrator-modify.geo.json")
-rs <- raster("script/figure1A/CN-201401_201412-res0.01_sum.tif")
-pt <- levelplot(rs, zscaleLog=TRUE, cuts = 99,
+rs <- raster("script/figure1A/CN-201401_201412-res0.01_sum.tif") %>%
+  aggregate(fact = c(3, 3), fun="mean", na.rm = T)
+pt <- levelplot(rs+1, zscaleLog=TRUE, #cuts = 12,
                 par.settings = mapTheme,
                 panel = function(...) {
                   panel.fill(col = "white")
@@ -25,10 +26,9 @@ pt <- levelplot(rs, zscaleLog=TRUE, cuts = 99,
                 scales=list(y=list(rot=90), tck=c(0.5,0.5)), 
                 margin = FALSE,
                 colorkey=list(space='right'))
-
-# detach(package:tidyverse, unload=TRUE)
 pt <- pt + layer(sp.polygons(bound, fill='transparent', col = "grey25", alpha = 0.6, lwd = 0.8))
-png("script/figure1A/CN-201401_201412-res0.01-white_sum.tif", width = 5000, height = 3500, res = 600)
+
+png("script/figure1A/Weibo_visual.tif", width = 5000, height = 3500, res = 600)
 print(pt)
 dev.off()
 
